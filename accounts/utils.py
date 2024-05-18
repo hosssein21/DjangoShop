a1185ec448ef9d161fc25a1726994fe3b599d893
hosssein <hossseinkazemi79@gmail.com>
2024-05-18 19:33:25 +0330

    managing seinding email with multi threading

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,4 +1,6 @@
 from threading import Thread
+from django.core.mail import send_mail
+
 
 class EmailThread(Thread):
     
@@ -8,3 +10,23 @@ class EmailThread(Thread):
         
     def run(self):
         self.email_obj.send()
+        
+class TemplateEmailThread(Thread):
+    def __init__(self, subject, html_message, plain_message, from_email, recipient_list):
+        self.subject = subject
+        self.html_message = html_message
+        self.plain_message = plain_message
+        self.from_email = from_email
+        self.recipient_list = recipient_list
+        Thread.__init__(self)
+    
+    def run(self):
+        send_mail(
+            subject=self.subject,
+            message=self.plain_message,
+            from_email=self.from_email,
+            recipient_list=self.recipient_list,
+            html_message=self.html_message,
+        )
+        
+        
--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,19 @@ from .forms import AuthenticationForm,SignUpForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views import View
-from .utils import EmailThread
+from .utils import EmailThread,TemplateEmailThread
 from mail_templated import EmailMessage
 from django.http import HttpResponse
 from django.contrib.messages.views import SuccessMessageMixin
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth import get_user_model
+
+
+
 
 
 class LoginView(auth_view.LoginView):
@@ -34,11 +43,55 @@ class TestEmailView(View):
         return HttpResponse('email sent successfully')
     
 
-class ResetPasswordView(SuccessMessageMixin, auth_view.PasswordResetView):
+class ResetPasswordView(auth_view.PasswordResetView):
     template_name = 'email/password-reset.html'
     email_template_name = 'email/password-reset-email.html'
     subject_template_name = 'email/password-reset-subject.txt'
     success_url = reverse_lazy('accounts:password_reset_done')
+    token_generator = default_token_generator
+    
+    
+    def form_valid(self, form):
+        """
+        If the form is valid, send the email using threading.
+        """
+        User = get_user_model()
+        email = form.cleaned_data['email']
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            # If user does not exist, silently ignore to avoid leaking information
+            return super().form_valid(form)
+
+        c = {
+            'email': user.email,
+            'domain': self.request.META['HTTP_HOST'],
+            'site_name': 'Django Shop',
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'user': user,
+            'token': default_token_generator.make_token(user),
+            'protocol': 'http' if not self.request.is_secure() else 'https',
+        }
+        subject = render_to_string(self.subject_template_name, c)
+        subject = ''.join(subject.splitlines())
+        html_message = render_to_string(self.email_template_name, c)
+        plain_message = strip_tags(html_message)
+        from_email = 'parto@gmail.com'
+        recipient_list = [user.email]
+
+        # Start a new thread for sending the email
+        TemplateEmailThread(subject, html_message, plain_message, from_email, recipient_list).start()
+        
+        return super().form_valid(form)
+    
+    def send_mail(self, subject_template_name, email_template_name,
+                  context, from_email, to_email, html_email_template_name=None):
+        """
+        Do nothing. Override this method to prevent Django from sending the email immediately.
+        """
+        pass
+    
+    
     
 class PasswordRestDoneView(auth_view.PasswordResetDoneView):
     template_name="email/password-reset-done.html"
