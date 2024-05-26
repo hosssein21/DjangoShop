fc6ff6da770eac27e32e8854b454a0899b638c78
hosssein <hossseinkazemi79@gmail.com>
2024-05-27 00:33:54 +0330

    crt view

--- cart/cart.py
+++ cart/cart.py
@@ -14,5 +14,25 @@ class CartSession:
         else:
             return
         self.save()
+        
+    def add_product(self, product_id):
+        for item in self._cart["items"]:
+            if product_id == item["product_id"]:
+                item["quantity"] += 1
+                break
+        else:
+            new_item = {"product_id": product_id, "quantity": 1}
+            self._cart["items"].append(new_item)
+        self.save()
+        
+    def get_cart_dict(self):
+        return self._cart
+        
+    def save(self):
+        self.session.modified = True
+        
+    def clear(self):
+        self._cart = self.session["cart"] = {"items": []}
+        self.save()
     
     
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,18 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
+from django.http import JsonResponse
+from shop.models import ProductModel, ProductStatusType
+from .cart import CartSession
 
-# Create your views here.
+class SessionAddProductView(View):
+
+    def post(self, request, *args, **kwargs):
+        cart = CartSession(request.session)
+        product_id = request.POST.get("product_id")
+        if product_id and ProductModel.objects.filter(id=product_id, status=ProductStatusType.publish.value).exists():
+
+            cart.add_product(product_id)
+        if request.user.is_authenticated:
+            cart.merge_session_cart_in_db(request.user)
+        return JsonResponse({"cart": cart.get_cart_dict(), "total_quantity": cart.get_total_quantity()})
