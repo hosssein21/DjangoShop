e697705d02de0b73bce8d8b99306a55a846ff5fc
hosssein <hossseinkazemi79@gmail.com>
2024-05-26 23:47:21 +0330

    adding session review in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.contrib.sessions.models import Session
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Profile
@@ -72,7 +72,15 @@ class CustomUserAdmin(UserAdmin):
 class CustomProfileAdmin(admin.ModelAdmin):
     list_display = ("id","user", "first_name","last_name","phone_number")
     searching_fields = ("user","first_name","last_name","phone_number")
+    
 
-
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return obj.get_decoded()
+    list_display = ['session_key', '_session_data', 'expire_date']
+    readonly_fields = ['_session_data']
+    
+    
+admin.site.register(Session, SessionAdmin)
 admin.site.register(Profile,CustomProfileAdmin)
 admin.site.register(User, CustomUserAdmin)
